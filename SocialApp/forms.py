eaeb4e43a541e926f7b2495c6fa8f023f384b96f
Mihaela Ciobotaru <mihaela.gabriela.93@gmail.com>
2016-01-16 15:19:24 +0200

    registration

--- SocialApp/forms.py
+++ SocialApp/forms.py
@@ -1,19 +1,20 @@
 from django.forms import Form, CharField, PasswordInput, DateField, ImageField
 from django.contrib.admin.widgets import AdminDateWidget
+from SocialApp.models import Role
 
 
 class UserForm(Form):
-    userName = CharField(max_length=30)
+    username = CharField(max_length=30)
+    password = CharField(widget=PasswordInput)
+
+
+class UserLoginForm(Form):
+    username = CharField(max_length=30)
     password = CharField(widget=PasswordInput)
 
 
 class UserRegisterForm(UserForm):
-    firstName = CharField(max_length=30)
-    lastName = CharField(max_length=30)
-    birthDate = DateField(widget=AdminDateWidget)
-    avatar = ImageField()
+    ROLE_CHOICES = ('Company', 'Client')
+    fullname = CharField(max_length=50)
+    select_role = CharField(max_length=30)
 
-
-class CompanyRegisterForm(UserForm):
-    name = CharField(max_length=30)
-    cover = ImageField()
